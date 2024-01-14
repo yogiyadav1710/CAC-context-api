08248aa24dd9ee6610bdc80b3855f5236dd961de
D5_56586-yogesh <yogiyadav1710@gmail.com>
2024-01-14 22:18:48 +0530

    Context api method 1 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import UserContextProvider from './context/UserContextProviders'
+import Profile from './components/Profile'
+import Login from './components/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <UserContextProvider>
       <h1>Learn Context API</h1>
-    </>
+      <Login />
+      <Profile />
+    </UserContextProvider>
   )
 }
 
